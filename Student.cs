0bd63fff0a3b71bafbf4b272ff693a73e68b4496
Vighnesh kumar <135311751+kvighnesh999@users.noreply.github.com>
2023-06-20 20:50:25 +0530

    updated files
    second commit

---
+++ Student.cs
@@ -0,0 +1,12 @@
+﻿namespace Student_Details
+{
+    public class Student
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }    
+        public int Age { get; set; }
+        public string Department { get; set; }
+        public string Class { get; set; }
+        public long Contact { get; set; }
+    }
+}
